07178bf7488dadc758bcc4dfd0c0fae18793d8f9
Ethan <113479501+EthanSc-o7@users.noreply.github.com>
2024-11-30 20:23:05 -0800

    swapped starfighter image and added stars and scores

--- Test.rb
+++ Test.rb
@@ -1,5 +1,9 @@
 require 'gosu'
 
+  module ZOrder 
+    BACKGROUND, STARS, PLAYER, UI = *0..3
+  end
+
  class Tutorial < Gosu::Window
   def initialize
     super 640,480
@@ -10,6 +14,10 @@ require 'gosu'
 
     @player2 = Player.new(2)
     @player2.warp(160,240)
+    @star_anim = Gosu::Image.load_tiles("media/star.png", 25,25)
+    @stars = Array.new
+    
+    @font = Gosu::Font.new(20)
   end 
 
   def update 
@@ -29,25 +37,38 @@ require 'gosu'
     
     end
     if @player2.getID == 2
-    if Gosu.button_down? Gosu::KB_A or Gosu::button_down? Gosu::GP_LEFT
-      @player2.turn_left
+      if Gosu.button_down? Gosu::KB_A or Gosu::button_down? Gosu::GP_LEFT
+        @player2.turn_left
+      end
+
+      if Gosu.button_down? Gosu::KB_D or Gosu::button_down? Gosu::GP_RIGHT
+        @player2.turn_right
+      end
+
+      if Gosu.button_down? Gosu::KB_W or Gosu::button_down? Gosu::GP_BUTTON_0
+        @player2.accelereate
+      end
+      @player2.move
     end
 
-    if Gosu.button_down? Gosu::KB_D or Gosu::button_down? Gosu::GP_RIGHT
-      @player2.turn_right
-    end
-
-    if Gosu.button_down? Gosu::KB_W or Gosu::button_down? Gosu::GP_BUTTON_0
-      @player2.accelereate
-    end
-    @player2.move
+    @player.collect_stars(@stars)
+    @player2.collect_stars(@stars) 
+    if rand(100) < 4 and @stars.size < 25 
+      @stars.push (Star.new(@star_anim))
+    end 
   end
-  end 
+ 
+
 
   def draw 
     @player2.draw
     @player.draw
     @background_image.draw(0,0,0)
+    @stars.each{|star| star.draw}
+    @font.draw_text("Score: #{@player.score}", 10, 10, ZOrder::UI,1.0,1.0, Gosu::Color::YELLOW)
+    @font.draw_text("Score: #{@player2.score}", 470, 10, ZOrder::UI,1.0,1.0, Gosu::Color::YELLOW)
+
+ 
   end 
 
   def button_down(id)
@@ -60,8 +81,10 @@ require 'gosu'
 end 
 
 class Player 
+  attr_reader :score
+
   def initialize(id)
-    @image = Gosu::Image.new("media/starfighter.bmp")
+    @image = Gosu::Image.new("media/susgreen.bmp")
     @x = @y = @vel_x = @vel_y = @angle = 0.0
     @score = 0
     @playerID = id
@@ -101,5 +124,42 @@ class Player
   def getID
     return @playerID
   end
+
+  def score 
+    @score 
+  end 
+
+  def collect_stars(stars)
+    stars.reject! do |star| 
+      if Gosu.distance(@x,@y,star.x,star.y) < 35
+        @score +=10
+        true
+      else 
+        false 
+      end
+    end
+  end 
 end 
+
+class Star 
+  attr_reader :x, :y 
+
+  def initialize(animation)
+    @animation = animation
+    @color = Gosu::Color::BLACK.dup
+    @color.red = rand(256-40) + 40
+    @color.green = rand(256-40) + 40
+    @color.blue = rand(256-40) + 40
+    @x = rand * 640
+    @y = rand * 480 
+  end 
+
+  def draw
+    img = @animation[Gosu.milliseconds / 100 % @animation.size]
+    img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
+    ZOrder::STARS, 1,1, @color, :add)
+  end
+end
+
+
 Tutorial.new.show
